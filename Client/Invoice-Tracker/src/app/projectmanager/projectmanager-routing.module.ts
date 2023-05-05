6cbacd34788ef44d0804d274a475b1ec41142b8b
fathimasulthanaas <fathimasulthanaas001@gmail.com>
2023-05-05 10:25:10 +0530

    pm-menubar

--- Client/Invoice-Tracker/src/app/projectmanager/projectmanager-routing.module.ts
+++ Client/Invoice-Tracker/src/app/projectmanager/projectmanager-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  {path:'', component: DashboardComponent}
+  {
+    path: '', component: HomepageComponent
+  }
 ];
 
 @NgModule({
